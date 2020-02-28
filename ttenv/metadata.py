a5b6179cebca62bed8864d5912233f31a8622c48
Heejin Jeong <heejinjeong@huntsman-ve703-03216.apn.wlan.private.upenn.edu>
2020-02-28 16:09:52 -0500

    metadatav5 range

--- ttenv/metadata.py
+++ ttenv/metadata.py
@@ -107,9 +107,9 @@ METADATA_v5={
         'target_init_cov': 30.0, # initial target diagonal Covariance.
         'target_init_vel': [0.0, 0.0], # target's initial velocity.
         'target_speed_limit': 3.0, # speed limit of targets.
-        'lin_dist_range_a2b':(5.0, 15.0),
+        'lin_dist_range_a2b':(5.0, 20.0),
         'ang_dist_range_a2b':(-np.pi, np.pi),
-        'lin_dist_range_b2t':(0.0, 15.0),
+        'lin_dist_range_b2t':(0.0, 20.0),
         'ang_dist_range_b2t':(-np.pi/2, np.pi/2),
         'margin': 1.0, # a marginal distance btw targets and the agent.
         'margin2wall': 0.5, # a marginal distance from a wall.
