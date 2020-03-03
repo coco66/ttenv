8d659f4eca75e4f69e52d9cd8d827dc02dd29182
Heejin Jeong <heejinjeong@huntsman-ve703-10015.apn.wlan.private.upenn.edu>
2020-03-03 12:57:10 -0500

    run_example update.

--- run_example.py
+++ run_example.py
@@ -10,7 +10,9 @@ parser.add_argument('--ros', help='whether to use ROS', type=int, default=0)
 parser.add_argument('--nb_targets', help='the number of targets', type=int, default=1)
 parser.add_argument('--log_dir', help='a path to a directory to log your data', type=str, default='.')
 parser.add_argument('--map', type=str, default="emptySmall")
-
+parser.add_argument('--repeat', type=int, default=1)
+parser.add_argument('--im_size', type=int, default=28)
+parser.add_argument('--seed', type=int, default=0)
 args = parser.parse_args()
 
 def main():
@@ -22,16 +24,20 @@ def main():
                     directory=args.log_dir,
                     num_targets=args.nb_targets,
                     is_training=False,
+                    im_size=args.im_size,
                     )
-    nlogdetcov = []
-    obs, done = env.reset(), False
-    while(not done):
-        if args.render:
-            env.render()
-        obs, rew, done, info = env.step(env.action_space.sample())
-        nlogdetcov.append(info['mean_nlogdetcov'])
 
-    print("Sum of negative logdet of the target belief covariances : %.2f"%np.sum(nlogdetcov))
+    np.random.seed(args.seed)
+    for _ in range(args.repeat):
+        nlogdetcov = []
+        obs, done = env.reset(), False
+        while(not done):
+            if args.render:
+                env.render()
+            obs, rew, done, info = env.step(env.action_space.sample())
+            nlogdetcov.append(info['mean_nlogdetcov'])
+
+        print("Sum of negative logdet of the target belief covariances : %.2f"%np.sum(nlogdetcov))
 
 if __name__ == "__main__":
     main()
