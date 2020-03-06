800a7048e1693ead0cbd37f8dcb67ac8280ddee5
Heejin Jeong <heejinjeong@Heejins-MBP.fios-router.home>
2020-03-05 20:48:12 -0500

    gen init pose and trajectory codes

--- ttenv/gen_trajectory.py
+++ ttenv/gen_trajectory.py
@@ -1,5 +1,6 @@
 """
-Generates a set of initial positions of targets and the agent in ttenv.
+Generates a set of initial positions of targets and the agent as well as target
+paths in ttenv.
 If you want to have more conditions to generate initial positions other than
 the current metadata version, provide values for the additional variables to
 the reset function. For example,
@@ -8,7 +9,7 @@ the reset function. For example,
                 'target_direction':False,
                 'belief_direction':False,
                 'blocked':True }
-    obs.reset(**ex_var)
+    env.reset(**ex_var)
 """
 import numpy as np
 import envs
@@ -21,9 +22,9 @@ parser.add_argument('--env', help='environment ID', default='TargetTracking-v1')
 parser.add_argument('--render', type=int, default=1)
 parser.add_argument('--map', type=str, default="obstacles02")
 parser.add_argument('--nb_targets', type=int, default=1)
-parser.add_argument('--nb_init_pose', type=int, default=10)
+parser.add_argument('--nb_paths', type=int, default=10)
 parser.add_argument('--log_dir', type=str, default='.')
-
+parser.add_argument('--eval_num', type=int, default=0)
 args = parser.parse_args()
 
 def main():
@@ -39,23 +40,31 @@ def main():
     while( not hasattr(timelimit_env, '_elapsed_steps')):
         timelimit_env = timelimit_env.env
     init_pose = []
-
-    params = {}
-    # This is an example. Please change this if necessary.
-    # from logger import TTENV_EVAL_SET
-    # params = TTENV_EVAL_SET[0]
-
-    while(len(init_pose) < args.nb_init_pose): # test episode
-        _, done = env.reset(**params), False
+    target_paths = []
+    from logger import TTENV_EVAL_SET
+    while(len(init_pose) < args.nb_paths): # test episode
+        _, done = env.reset(**TTENV_EVAL_SET[args.eval_num]), False
         if args.render:
             env.render()
-        notes = input("%d, Pass? y/n"%len(init_pose))
+        notes = input("%d, Init Pose Pass? y/n"%len(init_pose))
         if notes == "y":
-            init_pose.append({'agent':timelimit_env.env.agent.state,
+            init_pose_k = {'agent':timelimit_env.env.agent.state,
                             'targets':[timelimit_env.env.targets[i].state for i in range(args.nb_targets)],
-                            'belief_targets':[timelimit_env.env.belief_targets[i].state for i in range(args.nb_targets)]})
+                            'belief_targets':[timelimit_env.env.belief_targets[i].state for i in range(args.nb_targets)]}
+            target_path_t = [[]] * args.nb_targets
+            while not done:
+                _, _, done, _ = env.step(env.action_space.sample())
+                if args.render:
+                    env.render()
+                for i in range(args.nb_targets):
+                    target_path_t[i].append(timelimit_env.env.targets[i].state)
+            notes = input("%d, Pass? y/n"%len(init_pose))
+            if notes == "y":
+                init_pose.append(init_pose_k)
+                target_paths.append(target_path_t)
 
-    pickle.dump(init_pose, open(os.path.join(args.log_dir,'init_pose_random_1015.pkl'), 'wb'))
+    np.save(open(os.path.join(args.log_dir,'path_%d.npy'%args.eval_num), 'wb'), target_paths)
+    pickle.dump(init_pose, open(os.path.join(args.log_dir,'init_eval_%d.pkl'%args.eval_num), 'wb'))
 
 if __name__ == "__main__":
     main()
