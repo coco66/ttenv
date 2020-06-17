6139e933c74143d50005d6f41ad25522c21efe06
Heejin Jeong <heejinjeong@Heejins-MBP.fios-router.home>
2020-06-17 19:11:30 -0400

    added a previously used metadata set.

--- ttenv/metadata.py
+++ ttenv/metadata.py
@@ -1,7 +1,28 @@
 import numpy as np
 
+METADATA_v0={
+        'version' : 0,
+        'sensor_r': 10.0,
+        'fov' : 120,
+        'sensor_r_sd': 0.2, # sensor range noise.
+        'sensor_b_sd': 0.01, # sensor bearing noise.
+        'target_init_cov': 30.0, # initial target diagonal Covariance.
+        'target_init_vel': [0.0, 0.0], # target's initial velocity.
+        'target_speed_limit': 2.0, # velocity limit of targets.
+        'lin_dist_range_a2b':(0.0, 8.0),
+        'ang_dist_range_a2b':(-np.pi, np.pi),
+        'lin_dist_range_b2t':(0.0, 5.0),
+        'ang_dist_range_b2t':(-np.pi, np.pi),
+        'margin': 1.0, # a marginal distance btw targets and the agent.
+        'margin2wall': 0.5, # a marginal distance from a wall.
+        'action_v': [3, 2, 1, 0], # action primitives - linear velocities.
+        'action_w': [np.pi/2, 0, -np.pi/2], # action primitives - angular velocities.
+        'const_q': 0.01, # target noise constant in beliefs.
+        'const_q_true': 0.01, # target noise constant of actual targets.
+    }
+
 METADATA_v1={
-        'version' : 7,
+        'version' : 1,
         'sensor_r': 10.0,
         'fov' : 120,
         'sensor_r_sd': 0.2, # sensor range noise.
@@ -43,7 +64,7 @@ METADATA_multi_v1={
     }
 
 # Designate a metadata version to be used throughout the target tracking env.
-METADATA = METADATA_multi_v1
+METADATA = METADATA_v1
 
 TTENV_EVAL_SET = [
         { # Tracking
